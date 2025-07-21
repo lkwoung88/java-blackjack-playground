53949ef5c8cd477b94d300ed1e9cee65484c5225
lkwoung88 <lkwoung88@gmail.com>
2025-07-22 00:36:02 +0900

    feat : stream, lambda, optional

--- src/main/java/nextstep/fp/Lambda.java
+++ src/main/java/nextstep/fp/Lambda.java
@@ -1,8 +1,11 @@
 package nextstep.fp;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 public class Lambda {
+
     public static void printAllOld(List<Integer> numbers) {
         System.out.println("printAllOld");
 
@@ -17,6 +20,16 @@ public class Lambda {
         numbers.forEach(System.out::println);
     }
 
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        numbers.forEach(new Consumer<Integer>() {
+            public void accept(Integer value) {
+                System.out.println(value);
+            }
+        });
+    }
+
     public static void runThread() {
         new Thread(new Runnable() {
             @Override
@@ -27,27 +40,21 @@ public class Lambda {
     }
 
     public static int sumAll(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            total += number;
-        }
-        return total;
+        return sumAll(numbers, (x) -> true);
     }
 
     public static int sumAllEven(List<Integer> numbers) {
-        int total = 0;
-        for (int number : numbers) {
-            if (number % 2 == 0) {
-                total += number;
-            }
-        }
-        return total;
+        return sumAll(numbers, (x) -> x % 2 == 0);
     }
 
     public static int sumAllOverThree(List<Integer> numbers) {
+        return sumAll(numbers, (x) -> x > 3);
+    }
+
+    public static int sumAll(List<Integer> numbers, SumCondition condition) {
         int total = 0;
-        for (int number : numbers) {
-            if (number > 3) {
+        for (Integer number : numbers) {
+            if (condition.sumStrategy(number)) {
                 total += number;
             }
         }
---
+++ src/main/java/nextstep/fp/SumCondition.java
@@ -0,0 +1,5 @@
+package nextstep.fp;
+
+public interface SumCondition {
+    boolean sumStrategy(Integer number);
+}
